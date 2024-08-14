c88f9649af1afa92aae3fb445a92a94667ec60d9
vtAndreola <dev.andreola@gmail.com>
2024-08-14 16:37:54 -0300

    chore: update shadcn theme variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return <h1>Home page</h1>
+  return (
+    <h1>
+      Home page
+      <Button>Teste</Button>
+    </h1>
+  )
 }
