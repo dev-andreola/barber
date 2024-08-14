d589febffd046170422fc70f681988a9366a5890
vtAndreola <dev.andreola@gmail.com>
2024-08-14 17:17:18 -0300

    feat: create header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <Card className="rounded-t-none">
+      <CardContent className="flex items-center justify-between p-5">
+        <Image src={"/logo.png"} height={18} width={120} alt="FSW Barber" />
+        <Button variant={"outline"} size={"icon"}>
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
